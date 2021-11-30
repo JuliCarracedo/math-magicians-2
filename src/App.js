84e215feed0896232c2538e88a95ef54a1b7278b
JuliCarracedo <julicarra@hotmail.com>
2021-11-30 19:32:33 -0300

    add routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
 import React from 'react';
+import { BrowserRouter as Router, Routes, Switch } from 'react-router-dom';
+import Navbar from './components/Navbar';
 import Calculator from './components/Calculator';
+import Home from './components/Home';
 
 function App() {
   return (
-    <Calculator />
+    <React.StrictMode>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" render={() => <Home />} />
+          <Route path="/calc" render={() => <Calculator />} />
+        </Routes>
+      </Router>
+    </React.StrictMode>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Navbar = () => (
+  <nav className="navbar">
+    <h2> MATH MAGICIANS</h2>
+    <div>
+      <p>HOME</p>
+      <p>CALCULATOR</p>
+      <p>QUOTE</p>
+    </div>
+  </nav>
+);
+
+export default Navbar;
