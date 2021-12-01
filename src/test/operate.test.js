02082b4fffc3a6de108ae15d2445deed8927de3d
KARANGWA Ntwali Josue <kntwalijos@gmail.com>
2021-12-01 22:41:50 +0200

    Add test for operate function

---
+++ src/test/operate.test.js
@@ -0,0 +1,23 @@
+import operate from '../logic/operate';
+
+describe ('operate', () => {
+  it('add two numbers', () => {
+    expect(operate('1', '2', '+')).toBe('3');
+  });
+
+  it('subtract two numbers', () => {
+    expect(operate('3', '2', '-')).toBe('1');
+  });
+
+  it('multiply two numbers', () => {
+    expect(operate('3', '2', 'x')).toBe('6');
+  });
+
+  it('divide two numbers', () => {
+    expect(operate('6', '2', '÷')).toBe('3');
+  });
+
+  it('calculate the percentage', () => {
+    expect(operate('2', '100', '%')).toBe('2');
+  });
+});
